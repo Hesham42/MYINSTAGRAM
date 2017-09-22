132e588c038167dedf6ae1de5d52f3d4ec5f7553
hmostafa3bde <hmostafa3bde@outlook.com>
2017-09-22 02:52:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/heshammostafa/myinstagram/ApplicationData.java
@@ -0,0 +1,12 @@
+package com.example.heshammostafa.myinstagram;
+
+/**
+ * Created by HeshamMostafa on 7/29/2017.
+ */
+
+public class ApplicationData {
+    public static final String CLIENT_ID = "99ffee8a78ac4539b2ae107294f2c07f";
+    public static final String CLIENT_SECRET = "b969bb9dce344028a312acbfbcf2722f";
+    public static final String CALLBACK_URL = "instagram://connect";
+
+}
